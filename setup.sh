803f09fd4797bb866d4599daee536b5018204c53
Dominic Beauchamp <dox_88@hotmail.com>
2016-10-31 13:20:34 -0400

    add files

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo pacman -Syu
+sudo -S nodejs
+sudo -S npm
+sudo -S vim
+sudo -S tmux
+sudo -S htop
+sudo -S screenfetch
+sudo -S lolcat
+sudo -S git
+git clone https://github.com/doxkb/dotfiles ~/
